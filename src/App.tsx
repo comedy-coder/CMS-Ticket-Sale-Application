6a58fd888c2fc2a60ee42500888d93428d0f5213
Your Name <you@example.com>
2022-05-19 09:49:47 +0700

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
 import React from "react";
+import { publicRoutes } from "./routes";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import DefaultLayout from "./Layout/DefaultLayout";
 function App() {
   return (
-    <div className="App">
-      <h1> react-app</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {publicRoutes.map((route: any, index: any) => {
+            const Layout = route.layout || DefaultLayout;
+            const Page = route.component;
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                }
+              />
+            );
+          })}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/routes/index.ts
@@ -0,0 +1,11 @@
+import Home from "~/pages/Home";
+
+const publicRoutes : any =[
+    {
+        path : '/', component : Home 
+    }
+]
+const privateRoutes : any=[
+
+]
+export {publicRoutes,privateRoutes};
